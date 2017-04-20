f4c362fcadf5b24ac4a57e20a215ea97dc5246d5
Soundsphere <benedikt.rumpf@gmail.com>
2017-04-21 01:11:51 +0200

    Screenshot update
    Updated the screnshot

--- lastavg.py
+++ lastavg.py
@@ -26,6 +26,6 @@ play_count = stuff.xpath('//*[@id="content"]/div[2]/header/div[2]/div/div[2]/div
 clean_count = int(re.sub("[^\d\.]", "", play_count[0])) / int(delta.days + 1)
 
 # Display the stuff
-print ("Played Tracks: " + playcount[0])
+print ("Scrobbled Tracks: " + playcount[0])
 print ("Passed Days: " + str(int(delta.days + 1)))
 print ("Average: " + str("%.4f" % clean_count))
