5e914e73c3f082a5dafd0e1c25c67bde7f1d8832
Soundsphere <benedikt.rumpf@gmail.com>
2017-07-05 23:19:49 +0200

    very first working lastfm api call

--- lastapi.py
+++ lastapi.py
@@ -1,16 +1,18 @@
 #! python3
-# quickWeather.py - Prints the weather for a location from the command line.
 
 import json, requests
 import pprint
 
-# Get loved tracks
-lovedUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=StonedEars&api_key=269eeebb75b0c41507ec4601f66f92c9&format=json"
-loved = requests.get(lovedUrl)
-loved.raise_for_status()
+# Get user info
+infoUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=StonedEars&api_key=269eeebb75b0c41507ec4601f66f92c9&format=json"
+info = requests.get(infoUrl)
+info.raise_for_status()
 
 # Load JSON data into a Python variable.
-lovedData = json.loads(loved.text)
+infoData = json.loads(info.text)
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(lovedData)
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(infoData)
+
+pc = infoData["user"]
+print(pc["playcount"])
