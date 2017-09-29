18955e5237e75430b85cd781c2038db807dc6ffc
Soundsphere <benedikt.rumpf@gmail.com>
2017-09-29 17:47:01 +0200

    Create lastavg.sh
    Updates the stats every 15 minutes so that one can keep an eye on the stats. This usually runs somewhere in the background while I'm working

---
+++ lastavg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# I listen to music during work a lot. To keep an eye on the stats
+# I let this little script run in another window
+
+while true; do
+	clear
+	echo " _           _      __"
+	echo "| | __ _ ___| |_   / _|_ __ ___"
+	echo "| |/ _\` / __| __| | |_| '_ \` _ \\"
+	echo "| | (_| \\__ \\ |_ _|  _| | | | | |"
+	echo "|_|\\__,_|___/\\__(_)_| |_| |_| |_|"
+	echo "Refreshed: $(date +%H:%M), next refresh: $(date +%H:%M --date="900 seconds")"
+	python /home/bene/github/lastavg/lastavg.py
+	sleep 900
+done
